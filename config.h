0601bdfeb205b91ee3efa0542cf22ed1c62a3a34
Yurii Skrynnykov <truef1s7@gmail.com>
2022-02-14 02:47:32 +0200

    Change rofi config

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]           = { "rofi", "-show", "combi", NULL};
+static const char *dmenucmd[]           = { "rofi-menu", NULL};
 static const char *clipmenu[]           = { "clipmenu", NULL};
 static const char *screenshot[]         = { "screenshot", NULL};
 static const char *rofi_pass[]          = { "rofi-pass", NULL};
